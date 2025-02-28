c0fb424b14ec3488855ae359fe7a4855e204da3c
Hongren Zhu <hongren.zhu@dal.ca>
2025-02-27 20:38:04 -0400

    Added and documented a copy of the cars dataset

---
+++ R/data.R
@@ -0,0 +1,11 @@
+# cars Dataset
+#
+# This dataset contains the speed of cars and the distances taken to stop
+#
+# @format A data frame with 50 rows and 2 columns
+# \describe{
+#   \item{speed}{Speed of cars in mph}
+#   \item{dist}{Stopping distance in feet}
+# }
+
+"cars"
